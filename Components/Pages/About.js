55ed9a872ebef7660923ae56244e13c3c8b99a24
vinnyVani <90713453+vinnyVani@users.noreply.github.com>
2021-09-14 21:56:10 +0530

    Add files via upload

---
+++ Components/Pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./../Layout/sty.css";
+
+
+ 
+const About = () =>{
+
+    return (
+        <div>
+       
+            <img style={{width:"100%"}}src="./about.PNG"/>
+    </div>
+
+          
+    );
+}
+export default About;
